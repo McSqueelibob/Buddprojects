652092e3ade00bbb1f45d8e227ca8d6100668559
josh <jacobbudd2011@gmail.com>
2025-07-31 22:48:52 -0700

    take user input and save a json

--- sandbox/server.js
+++ sandbox/server.js
@@ -7,6 +7,19 @@ const port = 3000
 app.use(express.json());
 app.use(cors());
 
+app.post('/save-json-ts', (req, res) => {
+  const jsonData = req.body;
+  const timeStamp = jsonData.timeStamp;
+
+  fs.writeFile(timeStamp + '.json', JSON.stringify(jsonData, null, 2), (err) => {
+    if (err) {
+      console.error('Failed to write file:', err);
+      return res.status(500).send('Error writing file');
+    }
+    res.send('JSON data saved successfully!');
+  });
+});
+
 app.post('/save-json', (req, res) => {
   const jsonData = req.body;
 
